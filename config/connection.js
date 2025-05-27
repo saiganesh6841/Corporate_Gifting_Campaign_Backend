fe0f12b7ae62aae7de6d0c2b771328dedcdbf166
muhammed minhal <mhdminhal44@gmail.com>
2025-05-27 12:31:16 +0530

    aws changed for s3

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ module.exports = {
     accessKeyId: "AKIAZQ3DSLXZIJZUZK7N",
     secretAccessKey: "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",
     region: "ap-south-1", //ap-south-1  us-east-2
-    bucket: "clippetuploads",
+    bucket: "florauploads",
     link: "https://s3.ap-south-1.amazonaws.com/",
   },
 };
