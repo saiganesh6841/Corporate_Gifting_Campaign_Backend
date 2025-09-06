5adb8ec9d0410f39744030b0790ea858ba06fc69
AflahahamedPM <aflah777@gmail.com>
2025-09-06 17:12:29 +0530

    attendance api done for the admin side

--- api/controller/admin/AttendenceController.js
+++ api/controller/admin/AttendenceController.js
@@ -1,5 +1,6 @@
 const { returnCode } = require("../../../config/responseCode");
 const Attendance = require("../../model/Attendance");
+const User = require("../../model/User");
 const UtilController = require("../services/UtilController");
 
 module.exports = {
@@ -147,12 +148,16 @@ module.exports = {
       ];
       let result = await Attendance.aggregate(pipeline);
 
+      const userDetails = await User.findById(userObjectId).select(
+        "fullName email userType userId mobileNumber profileImage"
+      );
+
       UtilController.sendSuccess(req, res, next, {
         message: "Successfully fetched attendance details",
         responseCode: returnCode.validSession,
         result,
+        userDetails,
       });
-      console.log(result);
     } catch (error) {
       console.log("error: ", error);
       UtilController.sendError(req, res, next, error);
